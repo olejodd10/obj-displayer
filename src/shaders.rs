6f7bdb90804811a93cadd64ba14bc0e2e4af03a4
olejodd10 <67640511+olejodd10@users.noreply.github.com>
2023-01-05 21:17:54 +0100

    Moved shaders to src/shaders

--- src/shaders.rs
+++ src/shaders.rs
@@ -1,6 +1,6 @@
-pub const VERTEX_SHADER_SRC: &str = include_str!("../shaders/default.vert");
+pub const VERTEX_SHADER_SRC: &str = include_str!("shaders/default.vert");
 
-pub const DEFAULT_FRAGMENT_SHADER_SRC: &str = include_str!("../shaders/default.frag");
-pub const SIMPLE_FRAGMENT_SHADER_SRC: &str = include_str!("../shaders/simple.frag");
-pub const ONLY_DIFFUSE_FRAGMENT_SHADER_SRC: &str = include_str!("../shaders/diffuse_only.frag");
-pub const COMPLETE_FRAGMENT_SHADER_SRC: &str = include_str!("../shaders/complete.frag");
+pub const DEFAULT_FRAGMENT_SHADER_SRC: &str = include_str!("shaders/default.frag");
+pub const SIMPLE_FRAGMENT_SHADER_SRC: &str = include_str!("shaders/simple.frag");
+pub const ONLY_DIFFUSE_FRAGMENT_SHADER_SRC: &str = include_str!("shaders/diffuse_only.frag");
+pub const COMPLETE_FRAGMENT_SHADER_SRC: &str = include_str!("shaders/complete.frag");
